7ba95d14fc7d87f0c9bc4449bfadf2e4542804e0
RiteshKrPanda <riteshkrpandawork@gmail.com>
2025-10-30 22:14:37 +0530

    add the initial code

--- AnotherTodoApp.py
+++ AnotherTodoApp.py
@@ -1,1 +1,10 @@
+tasks = []
+answer = ""
+while answer != 2 :
+    answer = int(input("Choose Option "))
 
+    if answer == 1 :
+        task = input(f"Enter Your Task ")
+        tasks.append(task)
+    for task in tasks :
+        print(task)
